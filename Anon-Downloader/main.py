858f9a96a7ae13d41d430072a78b5bd1278e2ea1
elias <peanut110308@gmail.com>
2025-07-21 09:34:44 +0200

    Anon-Downloader angefangen

---
+++ Anon-Downloader/main.py
@@ -0,0 +1,3 @@
+#Manueller Download, but schnelle Umbenennung mit Daten aus X
+#Download mit yt-dlp, maybe meine Download-Methode noch mit rausfinden (automatisiert)
+#Klick-Downloads auch hinzufügen
